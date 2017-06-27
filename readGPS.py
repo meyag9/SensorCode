aa03a3d539000da55ca971d7689cd93d6d13aa23
Gorbea <mgorbea@csumb.edu>
2017-06-27 15:40:44 -0700

    made output to new txt file.. more readable for user

--- readGPS.py
+++ readGPS.py
@@ -4,22 +4,21 @@ import time
 import Queue
 import sys
 import time
+from pyproj import Proj
 
 
 def parseGPS(gps_msg):
 	if("$GPRMC" in gps_msg):
 		return gps_msg
 
-#def convertLat():
-
 
 if __name__ == "__main__":
 
 	f = open(sys.argv[1], "rb")
+	w = open("data.txt","w")
 
 	while True:
 		no_msg = True
-		#list1 = []
 
 		#gps_msg = gps.get_line()		# this is NOT blocking!
 		gps_msg = f.readline()
@@ -28,15 +27,17 @@ if __name__ == "__main__":
 		if gps_msg != None:
 			no_msg = False
 			tstr = time.strftime('%Y-%m-%d %H:%M:%S')
-		#print(tstr)
 			t = time.time()
 			msg = parseGPS(gps_msg)
 			if msg != None:
 				list1 = msg.split(",")
 				gpsType = list1[0]
-				time_ = (list1[1])
+				time_ = list1[1]
+				lat = list1[3] + list1[4]
+				lon = list1[5] + list1[6]
+				msgstr = tstr + " " + gpsType + " "+ lat + " " + lon
+				w.write(msgstr+"\n")
 				print (tstr, " : ", time_)
-		#	time.sleep(1)
 
 		if no_msg:
 			time.sleep(.05)
